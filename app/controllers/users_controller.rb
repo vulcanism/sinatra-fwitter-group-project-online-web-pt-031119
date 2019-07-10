e02dd0690b858d4a1d80931f448fc1d30bf43e78
Savannah C <sgcorrell@gmail.com>
2019-07-10 20:08:48 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
   
   post "/signup" do
+    if !logged_in
     if params[:username] != "" && params[:email] != "" && params[:password] != ""
       @user = User.new(params)
       @user.save
