9a29448d529b58e969bb5039d2cdd256f14c980a
Savannah C <sgcorrell@gmail.com>
2019-07-10 20:55:37 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   
   get "/users/:slug" do
     @user = User.find_by_slug(params[:slug])
-    
+    erb :"/users/show"
   end
   
   get "/logout" do
