a1dfac3fa6aa751ab0d6ce3aa8b53e3bf28df3f4
Savannah C <sgcorrell@gmail.com>
2019-07-10 21:39:18 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,53 @@
 class UsersController < ApplicationController
+  
+  get "/signup" do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :"/users/signup"
+    end
+  end
+  
+  post "/signup" do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.new(params)
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+  
+  get "/login" do
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect "/tweets"
+    end
+  end
+  
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+  
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :"/users/show"
+  end
+  
+  get "/logout" do
+    if logged_in?
+      session.destroy
+    end
+      redirect "/login"
+  end
 
 
 end
