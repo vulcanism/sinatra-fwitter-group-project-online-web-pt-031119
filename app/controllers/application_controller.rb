d006fd37088b11040472ddec04581be54f9815f5
Savannah C <sgcorrell@gmail.com>
2019-07-10 20:06:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     end
     
     def current_user
-      @current_user ||= User.find_by()
+      @current_user ||= User.find_by(session[:user_id])
     end
     
   end
